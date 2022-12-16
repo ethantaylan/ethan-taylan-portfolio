ea8c409eb3e4d82e668ca1d68ccb354072f85474
Ethan <ethtaylan@gmail.com>
2022-12-16 11:31:17 +0100

    navbar

--- src/components/Navbar/Navbar.tsx
+++ src/components/Navbar/Navbar.tsx
@@ -2,6 +2,9 @@ import { NavLink } from 'react-router-dom';
 import { GiHamburgerMenu } from 'react-icons/gi';
 import React from 'react';
 import './navbar.scss';
+import useMediaQuery from '../../hooks/useMediaQuery';
+import { IoIosClose } from 'react-icons/io';
+import Footer from '../Footer/Footer';
 
 const Navbar = () => {
 	const [toggleNavbar, setToggleNavbar] = React.useState<boolean>(false);
@@ -12,6 +15,8 @@ const Navbar = () => {
 		setToggleNavbar(false);
 	};
 
+	const matches = useMediaQuery('(min-width: 768px)');
+
 	return (
 		<React.Fragment>
 			<div className='navbar-container-main my-border-bottom'>
@@ -21,17 +26,52 @@ const Navbar = () => {
 						className='btn-burger hover'
 						onClick={toggleHandler}
 					>
-						<GiHamburgerMenu />
+						{toggleNavbar ? (
+							<div className='close-container d-flex justify-content-center align-items-center'>
+								<IoIosClose
+									// onClick={() => setIsDetailActive(false)}
+									size={30}
+									className='close-btn m-4'
+								/>
+							</div>
+						) : (
+							<GiHamburgerMenu />
+						)}
 					</span>
 				</div>
-
 				<ul
 					className={
 						toggleNavbar
-							? 'my-border-bottom align-items-center navbar-nav-ul'
+							? 'align-items-center navbar-nav-ul'
 							: 'd-768-none navbar-nav-ul'
 					}
 				>
+					{/* structuration error, need to reformat later */}
+					{matches ? (
+						''
+					) : (
+						<div className='navbar-container-main my-border-bottom'>
+							<div className='brand-and-contact-btn'>
+								<div className='navbar-container-brand'>ethan-taylan</div>
+								<span
+									className='btn-burger hover'
+									onClick={toggleHandler}
+								>
+						{toggleNavbar ? (
+							<div className='close-container d-flex justify-content-center align-items-center'>
+								{' '}
+								<IoIosClose
+									// onClick={() => setIsDetailActive(false)}
+									size={34}
+								/>
+							</div>
+						) : (
+							<GiHamburgerMenu />
+						)}
+								</span>
+							</div>
+						</div>
+					)}
 					<NavLink
 						className='link'
 						onClick={onClose}
@@ -60,7 +100,12 @@ const Navbar = () => {
 					>
 						<li className='navbar-nav-li'>_contact-me</li>
 					</NavLink>
+					<div style={{position: 'absolute', bottom: '0', width: '100%'}}>
+					<Footer />
+
+					</div>
 				</ul>
+
 				<div className='btn-contact'>
 					<span>_contact-me</span>
 				</div>
