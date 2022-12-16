d931435293f0eb62e21e3d966f80c99effc94244
Ethan <ethtaylan@gmail.com>
2022-12-16 13:33:10 +0100

    desktop mode not ready

--- src/components/DesktopNotReady/DesktopNotReady.tsx
+++ src/components/DesktopNotReady/DesktopNotReady.tsx
@@ -10,11 +10,12 @@ const DesktopNotReady = (props: Props) => {
 	return (
 		<div className={matches ? 'd-block' : 'd-none'}>
 			<div className='d-flex justify-content-center flex-column align-items-center container-full-page'>
-				<h1 style={{ color: 'white', marginBottom: '40px' }}>Desktop mode not ready :(</h1>
+				<h1 className='my-text-primary' style={{marginBottom: '30px', fontSize: '25px' }}>Desktop mode not ready :(</h1>
 				<div className='w-50'>
-				<h6 style={{textAlign:'end', color: 'gray' }}>73%</h6>
+				<h6 className='my-text-primary' style={{textAlign:'end'}}>73%</h6>
 					<ProgressBar now={73} />
 				</div>
+                <h6 className='my-text-primary mt-5'>// reduce your window size until you see the mobile version</h6>
 			</div>
 		</div>
 	);
